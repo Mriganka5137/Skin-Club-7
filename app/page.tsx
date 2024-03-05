e6e8d7d59719633054adf5ec07373de999dda2c9
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-03-05 07:48:55 +0530

    Update layout and page styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import TableContainer from "@/components/shared/TableContainer";
 
 export default function Home() {
   return (
-    <main className=" px-6 py-8 mx-auto max-2xl:w-full ">
+    <main className=" px-6 py-8 mx-auto w-full  border border-blue-400">
       <h1 className=" text-3xl font-light text-color--4C4D4F">Invoice</h1>
       <h3 className=" mt-2  text-color--4C4D4F opacity-85 text-lg">
         Invoice list
       </h3>
-      <div className="flex max-lg:flex-col gap-6 mt-8 ">
+      <div className="flex max-lg:flex-col gap-6 mt-8 w-full border border-pink-400 ">
         <TableContainer />
         <InvoiceContainer />
       </div>
--- components/shared/TableContainer.tsx
+++ components/shared/TableContainer.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 import { Separator } from "../ui/separator";
 import TableSearchbar from "./TableSearchbar";
 import DateFilter from "./DateFilter";
@@ -8,7 +7,7 @@ import DataTable from "./DataTable";
 
 const TableContainer = () => {
   return (
-    <section className=" bg-white rounded-[14px] max-lg:bg-transparent  max-lg:text-[12px] w-full xl:w-[750px]">
+    <section className=" bg-white rounded-[14px] max-lg:bg-transparent  max-lg:text-[12px] w-full xl:max-w-[1200px]">
       <div className="px-7 py-6 flex justify-between items-center max-sm:px-2 max-sm:py-2">
         <TableSearchbar />
         <DateFilter />
