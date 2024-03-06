b8695a4f23258fdbec9c4c9e59b8055ea4127e3b
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-03-06 10:24:44 +0530

    Update font style for invoice creation heading

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const EditInvoicePage = () => {
   return (
     <div className="  flex-grow w-full px-6 py-8 ">
       <h1 className=" text-3xl font-light text-color--4C4D4F">Payment</h1>
-      <h3 className=" mt-2  text-color--4C4D4F opacity-85 text-[20px]">
+      <h3 className=" mt-2  text-color--4C4D4F  font-light opacity-85 text-[20px]">
         Create a invoice for your patients
       </h3>
       <div className="flex flex-col lg:flex-row gap-6 mt-8 w-full max-sm:mt-2 ">
